da0792d366550805802f313799b208603cebf2d6
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2022-09-29 02:30:25 +0600

    hw 8 v2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('[type="email"]');
+const messageInput = form.querySelector('[name="message"]');
+
+const localValues = localStorage.getItem('feedback-form-state');
+if (localValues) {
+  const { email, message } = JSON.parse(localValues);
+  emailInput.value = email;
+  messageInput.value = message;
+}
+
+form.addEventListener(
+  'input',
+  throttle(() => {
+    const formValues = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formValues));
+  }, 500)
+);
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const localValues = localStorage.getItem('feedback-form-state');
+  console.log(JSON.parse(localValues));
+
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+});
